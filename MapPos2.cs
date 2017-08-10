399a4741bebd1417fe62b480185855a3b8e79e5e
fenghao54 <2275704338>
2017-08-10 16:19:52 +0800

    广度优先搜索

---
+++ MapPos2.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class MapPos2 : MonoBehaviour {
+   
+    public GameObject box;
+    public GameObject star;
+    public GameObject target;
+    public int count;
+    public int removeCount;
+    public int[,] map;//用来记录地图的信息
+    public Pos[,] sPos;//用来储存每个格子的步数
+    List<Pos> SearchPos;//用来记录同样步数的坐标
+    //public int FloorMask;
+    public int step = 1;
+    
+    void Start ()
+    {
+        map = new int[30, 30];
+        sPos = new Pos[30, 30];//用来记录步数
+        SearchPos = new List<Pos>();//实例化一个列表
+        ReadMapFile();
+        Draw();
+    }
+ 
+    void Update()
+    {
+        DealList();
+    }
+    public void ReadMapFile()//读取TXT文件
+    {
+        string path = Application.dataPath + "//" + "map2.txt";
+        if (!File.Exists(path))
+        {
+            return;
+        }
+
+        FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+        StreamReader read = new StreamReader(fs, Encoding.Default);
+        string strReadline;
+        int y = 0;
+
+        while ((strReadline = read.ReadLine()) != null)
+        {
+            for (int x = 0; x < strReadline.Length; ++x)
+            {
+
+                if (strReadline[x] == '1')
+                {
+                    map[x, y] = 1;//墙的标记
+                }
+                if (strReadline[x] == '8')
+                {
+                    map[x, y] = 8;//开始的位置
+                    sPos[x, y] = new Pos(x, y); //把坐标写到Pos类型的变量中;
+                    sPos[x, y].step = 1;//记录开始的步数是1
+                    SearchPos.Add(sPos[x, y]);
+                }
+                if (strReadline[x] == '9')
+                {
+                    map[x, y] = 9;//结束的位置
+                }
+
+
+            }
+            y += 1;
+        }
+    }
+    void Draw()
+    {
+       
+        for (int i = 0; i < 30; i++)
+        {
+            for (int j = 0; j < 30; j++)
+            {
+                if (map[i, j] == 1)//画出墙
+                {
+                    Instantiate(box, new Vector3(i, 0.5f, -j), Quaternion.identity);
+                    Debug.Log(11);
+                }
+                if (map[i, j] == 8)//画出起点
+                {
+                    Instantiate(star, new Vector3(i, 0.5f, -j), Quaternion.identity);
+                    Debug.Log(11);
+                }
+                if (map[i, j] == 9)//画出结束点
+                {
+                    Instantiate(star, new Vector3(i, 0.5f, -j), Quaternion.identity);
+                }
+            }
+        }
+    }
+    //处理列表里面的步数
+    void DealList()
+    {
+        Debug.Log(SearchPos[0].step);
+        int x;//用来记录横坐标
+        int y;//用来记录纵坐标
+
+        if (SearchPos.Count >= 1)
+        {
+           
+            x=SearchPos[0].X ;
+            y=SearchPos[0].Y ;
+            
+           
+            if (map[x + 1, y] == 9 || map[x - 1, y] == 9 || map[x, y - 1] == 9 || map[x, y + 1] == 9)
+            {
+                Debug.Log("END");
+                return;
+            }
+            if (map[x + 1, y] !=1 && sPos[x + 1, y]==null)
+            {
+                sPos[x + 1, y] = new Pos(x + 1, y);
+                sPos[x + 1, y].step = sPos[x, y].step + 1;
+                SearchPos.Add(sPos[x + 1, y]);
+                Instantiate(target, new Vector3(x+1, 0f, -y), Quaternion.identity);
+            }
+            if (map[x - 1, y] != 1 && sPos[x - 1, y]==null)
+            {
+                sPos[x - 1, y] = new Pos(x - 1, y);//实例化
+                sPos[x - 1, y].step = sPos[x, y].step + 1;
+                SearchPos.Add(sPos[x - 1, y]);//增加到待处理列表
+                Instantiate(target, new Vector3(x -1, 0f, -y), Quaternion.identity);
+            }
+            if (map[x , y-1] != 1 && sPos[x , y-1]==null)
+            {
+                sPos[x, y - 1] = new Pos(x, y - 1);
+                sPos[x , y-1].step = sPos[x, y].step + 1;
+                SearchPos.Add(sPos[x , y-1]);
+                Instantiate(target, new Vector3(x , 0f, 1-y), Quaternion.identity);
+            }
+            if (map[x , y+1] != 1 && sPos[x , y+1]==null)
+            {
+                sPos[x, y + 1] = new Pos(x, y + 1);
+                sPos[x , y+1].step = sPos[x, y].step + 1;
+                SearchPos.Add(sPos[x , y+1]);
+                Instantiate(target, new Vector3(x, 0f, -y-1), Quaternion.identity);
+            }
+            SearchPos.RemoveAt(0);
+          
+            
+        }
+        
+    }
+}
---
+++ MapPrint.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+
+public class MapPrint : MonoBehaviour
+{
+
+    //public wall wall;
+    public GameObject box;
+    public GameObject star;
+    public GameObject target;
+    
+    public  string[,] map;
+    public  int[,] sPos;
+    public int FloorMask;
+    public int step=1;
+    void Start()
+    {
+        map = new string[100, 100];
+        sPos = new int[100, 100];//用来记录步数
+        ReadMapFile();
+        FloorMask = LayerMask.GetMask("floor");
+        Draw();
+        SetSmap(step);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Setmap();
+        }
+        //StartCoroutine(SetSmap(step));
+        SetSmap(step);
+        //Debug.Log("bushu" + sPos[20, 20]);
+        step++;
+        for (int i = 1; i < 100; i++)
+        {
+            for (int j = 1; j < 100; j++)
+            {
+                if (sPos[i, j] == step)
+                {
+                    Debug.Log("X=" + i + "Y=" + j);
+                }
+            }
+        }
+        
+    
+    }
+
+    public void ReadMapFile()
+    {
+        string path = Application.dataPath + "//" + "map.txt";
+        if (!File.Exists(path))
+        {
+            return;
+        }
+
+        FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+        StreamReader read = new StreamReader(fs, Encoding.Default);
+        string strReadline;
+        int y = 0;
+
+        while ((strReadline = read.ReadLine()) != null)
+        {
+            for (int x = 0; x < strReadline.Length; ++x)
+            {
+                
+                if (strReadline[x] == '#')
+                {
+                    map[x, y] = "wall";
+                }
+                if (strReadline[x] == '^')
+                {
+                    map[x, y] = "star";
+                    sPos[x, y] = 1;
+                }
+                if (strReadline[x] == '*')
+                {
+                    map[x, y] = "stop";
+                    
+                }
+
+
+            }
+            y += 1;
+        }
+     
+    }
+    
+    public void Setmap()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, FloorMask))
+        {
+            Vector3 mousemap = hit.point;
+            Instantiate(target, mousemap, Quaternion.identity);
+        }
+    }
+
+    void Draw()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j = 0; j < 50; j++)
+            {
+                if (map[i, j] == "wall")
+                {
+                    Instantiate(box, new Vector3(-i, 0.5f, j), Quaternion.identity);
+                    
+                }
+                if (map[i, j] == "star")
+                {
+                    
+                    Instantiate(star, new Vector3(-i, 0.5f, j), Quaternion.identity);
+                    
+                    
+                }
+                if (map[i, j] == "stop")
+                {
+                    Instantiate(star, new Vector3(-i, 0.5f, j), Quaternion.identity);
+                    
+                    
+                }
+
+            }
+           
+        }
+       
+    }
+    void SetSmap(int n)//寻找自己周围的四个坐标 n是步数
+    {
+        for (int i = 1; i <100; i++)
+        {
+            for (int j = 1; j <100; j++)
+            { 
+                if (sPos[i, j] == n)
+                {
+                    if (map[i - 1, j] == "stop" || map[i + 1, j] == "stop" || map[i, j - 1]=="stop" || map[i, j + 1]=="stop")
+                    {
+                       return;
+                    }
+                    if (map[i - 1, j] == null)
+                    {
+                        sPos[i - 1, j] = (n + 1);
+                        Instantiate(target, new Vector3(-i+1, 0f, j), Quaternion.identity);
+                        //yield return new WaitForSeconds(0.01f);
+                    }
+                    if (map[i + 1, j] == null)
+                    {
+                        sPos[i + 1, j] = (n + 1);
+                        Instantiate(target, new Vector3(-i -1, 0f, j), Quaternion.identity);
+                        //yield return new WaitForSeconds(0.01f);
+                    }
+                    if (map[i , j-1] == null)
+                    {
+                        sPos[i , j-1] = (n + 1);
+                        Instantiate(target, new Vector3(-i , 0f, j-1), Quaternion.identity);
+                        //yield return new WaitForSeconds(0.01f);
+                    }
+                    if (map[i , j+1] == null)
+                    {
+                        sPos[i , j+1] = (n + 1);
+                        Instantiate(target, new Vector3(-i, 0f, j+1), Quaternion.identity);
+                        //yield return new WaitForSeconds(0.01f);
+                    }
+                }
+            }
+        }
+        //SetSmap(n + 1);
+        //yield return null;
+    }
+    
+}
